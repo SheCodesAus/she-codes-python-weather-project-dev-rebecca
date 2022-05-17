be156d89d48f1487bbd473073ac1f798b1680609
Rebecca Mansell <rebeccakatemansell@gmail.com>
2022-05-17 22:17:13 +1000

    2 more solutions competed

--- weather.py
+++ weather.py
@@ -26,17 +26,10 @@ def convert_date(iso_string):
     Returns:
         A date formatted like: Weekday Date Month Year e.g. Tuesday 06 July 2021
     """
-pass
-#     datetime.strptime(iso_string, "%d")
-
-# 2021-07-05T07:00:00+08:00
-
-# import datetime
-# test = datetime.datetime.strptime('2021-07-05T07:00:00+08:00', "%Y-%m-%dT%H:%M:%S%z")
-
-# print(test)
-
-# new = datetime.datetime.strptime(test, "%Y-%m-%dT%H:%M:%S%z")
+    data = iso_string[:-15] # Remove timezone at end
+    date_object = datetime.strptime(data, "%Y-%m-%d")
+    formatted = date_object.strftime("%A %d %B %Y")
+    return formatted
 
 def convert_f_to_c(temp_in_farenheit):
     """Converts an temperature from farenheit to celcius.
@@ -81,8 +74,25 @@ def load_data_from_csv(csv_file):
     Returns:
         A list of lists, where each sublist is a (non-empty) line in the csv file.
     """
-    pass
 
+    list = []
+    new_list = []
+
+    with open(csv_file) as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            list.append(row)
+
+    list.pop(0)
+
+    for row in list:
+        new_list.append(int(row[1]))
+        new_list.append(int(row[2]))
+
+    print(new_list)
+    return list
+
+print(load_data_from_csv("tests/data/example_one.csv"))
 
 def find_min(weather_data):
     """Calculates the minimum value in a list of numbers.
@@ -93,18 +103,20 @@ def find_min(weather_data):
         The minium value and it's position in the list.
     """
 
-    index = 0
-    minimum = weather_data[0]
+    if weather_data == []:
+        return()
+
+    list = []
 
     for num in weather_data:
-        if num <= minimum:
-            index += 1
-            minimum = num
+        num = float(num)
+        list.append(num)
 
-    return minimum, index
-    
-# data = [49, 57, 56, 55, 53]
-# print(find_min(data))
+    minimum = min(list)
+
+    for i in reversed(range(len(list))):
+        if list[i] == minimum:
+            return minimum, i
 
 def find_max(weather_data):
     """Calculates the maximum value in a list of numbers.
